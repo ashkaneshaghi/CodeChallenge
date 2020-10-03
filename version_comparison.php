776e53801b44ef5c7ce035edd1ea18fe277a0380
ashkan.ee@mail.com <45358130+ashkaneshaghi@users.noreply.github.com>
2020-10-03 13:23:48 +0200

    Error Fixed in test class

--- version_comparison.php
+++ version_comparison.php
@@ -1,10 +1,10 @@
 <?PHP
 class VersionComparison {
-    
+
     private $version_change_point = "1.0.17+60";
     
 	public function compareVersion($version) {
-		if (version_compare($version, $this->version_change_point) > 0) {
+		if (version_compare($version, $this->version_change_point) >= 0) {
 			return true;
 		} else {
 			return false;
